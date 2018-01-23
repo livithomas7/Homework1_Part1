39dcf1012c0167953dfe7f9685a451ce89750b15
livithomas7 <livithomas7@gmail.com>
2018-01-23 05:01:38 +0000

    Added information to Part1

---
+++ Part1.rb
@@ -0,0 +1,26 @@
+require 'ruby_intro.rb'
+
+def sum (array)
+  array.reduce 0, :+
+end
+
+def max_2_sum(array)
+	return 0 if a.empty?
+	return array[0] if array.length == 1
+	ar = array.sort
+	ar[-1] + ar[-2]
+end
+
+def sum_to_n?(array, n)
+	return false if array.empty? or array.length == 1
+
+	h = Hash.new
+	array.each{|x| 
+		if h.key? x
+			return true
+		else
+			h[n - x] = n	
+		end
+	}
+	return false
+end
